fdd7a5771d51929c43f6a408665e5becb119dbff
Eduardo Fernandes <edufernandes.1895@gmail.com>
2023-12-07 22:55:49 -0300

    Rotas de alunos

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,8 +6,7 @@ import loginRequired from '../middlewares/loginRequired';
 const router = new Router();
 
 // Não deveria existir -> Falha de segurança
-
-router.get('/', userController.index); // Lista usuários
+// router.get('/', userController.index); // Lista usuários
 // router.get('/:id', userController.show); // Lista usuário
 
 router.post('/', userController.store);
