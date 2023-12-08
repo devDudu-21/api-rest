3baaf6b008244b5462829156b6693a7511f3e5f8
Eduardo Fernandes <edufernandes.1895@gmail.com>
2023-12-07 22:21:47 -0300

    Corrigindo falhas de segurança para usuários

--- src/middlewares/loginRequired.js
+++ src/middlewares/loginRequired.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (req, res, next) => {
+export default async (req, res, next) => {
   const { authorization } = req.headers;
+
   if (!authorization) return res.status(401).json({ errors: ['Login Required'] });
 
   const [, token] = authorization.split(' ');
@@ -9,6 +11,16 @@ export default (req, res, next) => {
   try {
     const dados = jwt.verify(token, process.env.TOKEN_SECRET);
     const { id, email } = dados;
+
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) { return res.status(401).json({ errors: ['Usuário inválido'] }); }
+
     req.userId = id;
     req.userEmail = email;
     return next();
